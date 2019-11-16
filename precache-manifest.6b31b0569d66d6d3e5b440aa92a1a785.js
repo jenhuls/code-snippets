dd2dc26783cc9e6fcb8d7ecf7133dca1121e84c1
Jen Huls <jen@trivision.com>
2019-11-16 09:46:04 -0500

    Updates

---
+++ precache-manifest.6b31b0569d66d6d3e5b440aa92a1a785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9aaaf472ede3df494e102b34e5f9ed",
+    "url": "/code-snippets/index.html"
+  },
+  {
+    "revision": "3e4c674bb47ee5b7179f",
+    "url": "/code-snippets/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3e7df682d51ff6e6a22b",
+    "url": "/code-snippets/static/js/2.df737ea1.chunk.js"
+  },
+  {
+    "revision": "3e4c674bb47ee5b7179f",
+    "url": "/code-snippets/static/js/main.8adf3817.chunk.js"
+  },
+  {
+    "revision": "0baeb13eb60743b2d920",
+    "url": "/code-snippets/static/js/runtime-main.1adbca92.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/code-snippets/static/media/logo.25bf045c.svg"
+  }
+]);
